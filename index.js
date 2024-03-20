46ad17d1a43b6cd2d91423a4f9e0200a4344e5da
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-03-20 20:53:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Please select valid operator");
+}
+;
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
   { message: "Enter first number", type: "number", name: "firstnumber" },
   { message: "Enter second number", type: "number", name: "secondnumber" },
   {
-    message: "Select one of the operators to perform operation",
+    message: "Select one of the operators to perform action",
     type: "list",
     name: "operator",
     choices: ["Addition", "Subtraction", "Multiplication", "Division"],
